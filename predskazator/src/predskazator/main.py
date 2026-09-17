3b62d147b9bb1abf4720fdf2bf350aeee7bf1d97
Aglaya_Shevyrova <anshevyrova@edu.hse.ru>
2026-09-17 21:12:55 +0300

    Первый коммит: библиотека predskazator

---
+++ predskazator/src/predskazator/main.py
@@ -0,0 +1,15 @@
+from random import randint
+
+
+def predict() -> str:
+    a = randint(1, 5)
+    if a == 1:
+        return "предсказываю вам сегодня удачный день"
+    elif a == 2:
+        return "сегодня вам надо быть внимательнее"
+    elif a == 3:
+        return "⎛⎝( ` ᢍ ´ )⎠⎞ᵐᵘʰᵃʰᵃ"
+    elif a == 4:
+        return "рядом предатель ඞ ඞ ඞ ඞ ඞ ඞ ඞ"
+    else:
+        return "погладь сегодня кота ᓚᘏᗢ"
